60b35f7b43da001c439a2bad7e89f82a2af058cf
Mauro Silva <mauro.silva@pipefy.com>
2023-11-10 15:19:29 -0300

    fix - adjusting identify column of partitioned orders table

--- maintenance/rename_table_copy_data.sql
+++ maintenance/rename_table_copy_data.sql
@@ -5,4 +5,8 @@ ALTER TABLE orders_partitioned RENAME TO orders;
 
 INSERT INTO orders SELECT * FROM orders_old FOR UPDATE;
 
+SELECT max(id) as orders_old_max_id from orders_old \gset
+
+ALTER TABLE orders ALTER column id SET START :orders_old_max_id;
+
 COMMIT;
